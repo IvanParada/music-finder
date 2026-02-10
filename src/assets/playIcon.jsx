e2e888be0f4c749d9b864b88b01e723ad143cc4c
nsqws <118088453+IvanParada@users.noreply.github.com>
2026-02-10 01:29:43 -0300

    add icons

---
+++ src/assets/playIcon.jsx
@@ -0,0 +1,6 @@
+export default function PlayIcon() {
+
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24"><path fill="#EB6FE1" fillOpacity={0} stroke="#EB6FE1" strokeDasharray={38} strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 6l10 6l-10 6Z"><animate fill="freeze" attributeName="stroke-dashoffset" dur="0.5s" values="38;0"></animate><animate fill="freeze" attributeName="fill-opacity" begin="0.5s" dur="0.15s" to={0.3}></animate></path></svg>
+    );
+}
